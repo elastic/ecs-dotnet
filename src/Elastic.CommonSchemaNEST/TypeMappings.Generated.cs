2e1de8638df0ce888406fffabf7fba4529dbe87e
Martijn Laarman <Mpdreamz@gmail.com>
2019-11-28 11:44:38 +0100

    use abbreviated header file everywhere

--- src/Elastic.CommonSchemaNEST/TypeMappings.Generated.cs
+++ src/Elastic.CommonSchemaNEST/TypeMappings.Generated.cs
@@ -1,24 +1,11 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	 http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
 
 /*
 IMPORTANT NOTE
 ==============
-This file has been generated. 
+This file has been generated.
 If you wish to submit a PR please modify the original csharp file and submit the PR with that change. Thanks!
 */
 
@@ -59,7 +46,7 @@ namespace Elastic.CommonSchema.Elasticsearch
 			indexTemplate.IndexPatterns("ecs-*");
 			indexTemplate.Order(1);
 			indexTemplate.Settings(s =>
-				s.Setting("index", 
+				s.Setting("index",
 					new
 					{
 						refresh_interval = "5s",
@@ -73,7 +60,7 @@ namespace Elastic.CommonSchema.Elasticsearch
 					}));
 
 			indexTemplate.Map(GetTypeMappingDescriptor());
-			
+
 			return indexTemplate;
 		}
 
--- src/Generator/FileGenerator.cs
+++ src/Generator/FileGenerator.cs
@@ -1,3 +1,7 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -6,8 +10,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using CsQuery.ExtensionMethods;
 using Generator.Schema;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Newtonsoft.Json;
 using RazorLight;
 using RazorLight.Razor;
