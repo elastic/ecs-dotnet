2d75f35e52ad84b52acb24be2322f58f674844b0
Gergely Kalapos <gergo@kalapos.net>
2019-12-04 15:52:12 +0100

    Add Serilog enricher to append APM transaction and trace id to logs (#10)
    * Add Serilog enricher to append Elastic APM transaction and trace id to logs
    
    * Move tests to tests folder and rename to WithElasticApmCorrelationInfo
    
    * Create readme.md
    
    * Update readme.md

---
+++ src/Elastic.Apm.SerilogEnricher/ElasticApmEnricher.cs
@@ -0,0 +1,23 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Elastic.Apm.SerilogEnricher
+{
+	/// <summary>
+	/// This enricher adds trace id and the transaction id for every log that is created during a transaction in case
+	/// the Elastic APM Agent is active in your application.
+	/// </summary>
+	public sealed class ElasticApmEnricher : ILogEventEnricher
+	{
+		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+		{
+			if (!Agent.IsConfigured) return;
+			if (Agent.Tracer.CurrentTransaction == null) return;
+
+			logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+				"TransactionId", Agent.Tracer.CurrentTransaction.Id));
+			logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+				"TraceId", Agent.Tracer.CurrentTransaction.TraceId));
+		}
+	}
+}
---
+++ tests/Elastic.Apm.SerilogEnricher.Test/SerilogTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Elastic.Apm.Api;
+using Elastic.Apm.Report;
+using FluentAssertions;
+using Serilog;
+using Serilog.Sinks.InMemory;
+using Xunit;
+
+namespace Elastic.Apm.SerilogEnricher.Test
+{
+	public class SerilogTests : IDisposable
+	{
+		public SerilogTests()
+		{
+			if (!Agent.IsConfigured)
+				Agent.Setup(new AgentComponents(payloadSender: new NoopPayloadSender()));
+		}
+
+		/// <summary>
+		/// Creates 1 simple transaction and makes sure that the log line created within the transaction has
+		/// the transaction and trace ids, and logs prior to and after the transaction do not have those.
+		/// </summary>
+		[Fact]
+		public void SerilogEnricherWithSimpleSyncTransaction()
+		{
+			var logger = new LoggerConfiguration()
+				.Enrich.WithElasticApmCorrelationInfo()
+				.WriteTo.InMemory()
+				.CreateLogger();
+
+			string traceId = null;
+			string transactionId = null;
+
+			logger.Information("Line1");
+
+			Agent.Tracer.CaptureTransaction("Test", "Test", (t) =>
+			{
+				traceId = t.TraceId;
+				transactionId = t.Id;
+				logger.Information("Line2");
+			});
+
+			logger.Information("Line3");
+
+			InMemorySink.Instance
+				.LogEvents.Should()
+				.HaveCount(3);
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(0)
+				.Properties.Should()
+				.BeEmpty();
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(1)
+				.Properties["TraceId"]
+				.ToString()
+				.Should()
+				.Be($"\"{traceId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(1)
+				.Properties["TransactionId"]
+				.ToString()
+				.Should()
+				.Be($"\"{transactionId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(2)
+				.Properties.Should()
+				.BeEmpty();
+		}
+
+		/// <summary>
+		/// Same as <see cref="SerilogEnricherWithSimpleSyncTransaction" />, but this time in an async method
+		/// </summary>
+		/// <returns></returns>
+		[Fact]
+		public async Task SerilogEnricherWithSimpleAsyncTransaction()
+		{
+			var logger = new LoggerConfiguration()
+				.Enrich.WithElasticApmCorrelationInfo()
+				.WriteTo.InMemory()
+				.CreateLogger();
+
+			string traceId;
+			string transactionId;
+
+			logger.Information("Line1");
+
+			ITransaction transaction = null;
+
+			try
+			{
+				transaction = Agent.Tracer.StartTransaction("Test", "Test");
+				traceId = transaction.TraceId;
+				transactionId = transaction.Id;
+				logger.Information("Line2");
+				await Task.Delay(10);
+				logger.Information("Line3");
+			}
+			finally
+			{
+				transaction?.End();
+			}
+
+			logger.Information("Line4");
+
+			InMemorySink.Instance
+				.LogEvents.Should()
+				.HaveCount(4);
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(0)
+				.Properties.Should()
+				.BeEmpty();
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(1)
+				.Properties["TraceId"]
+				.ToString()
+				.Should()
+				.Be($"\"{traceId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(1)
+				.Properties["TransactionId"]
+				.ToString()
+				.Should()
+				.Be($"\"{transactionId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(2)
+				.Properties["TraceId"]
+				.ToString()
+				.Should()
+				.Be($"\"{traceId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(2)
+				.Properties["TransactionId"]
+				.ToString()
+				.Should()
+				.Be($"\"{transactionId}\"");
+
+			InMemorySink.Instance
+				.LogEvents.ElementAt(3)
+				.Properties.Should()
+				.BeEmpty();
+		}
+
+		public void Dispose() => InMemorySink.Instance.Dispose();
+	}
+
+	internal class NoopPayloadSender : IPayloadSender
+	{
+		public void QueueError(IError error) { }
+
+		public void QueueMetrics(IMetricSet metrics) { }
+
+		public void QueueSpan(ISpan span) { }
+
+		public void QueueTransaction(ITransaction transaction) { }
+	}
+}
