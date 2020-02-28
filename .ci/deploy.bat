f60faf27d57e75b58795ee9e85051deed75aaef5
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-28 17:13:17 +0000

    No more dry run

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -4,6 +4,5 @@ set url=%2
 
 @echo on
 FOR /R ".\" %%a  in (*.nupkg) DO (
-	@echo Filename: %%a
-	echo dotnet nuget push "%%a" -k "%key%" -s "%url%"
+	dotnet nuget push "%%a" -k "%key%" -s "%url%"
 )
