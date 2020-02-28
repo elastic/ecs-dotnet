e3ad505c740277216c119a91d8d30fc50a3ff408
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-28 16:24:57 +0000

    ci(jenkins): fix the command

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -3,5 +3,6 @@ set arg1=%1
 set arg2=%2
 
 @echo on
-for /r %%x in (.\build\output\_packages\*.nupkg) do echo "%%x"
-echo dotnet nuget push blabla -k ${arg1} -s ${arg2}
+FOR /R ".\" %%a  in (*.nupkg) DO (
+	echo dotnet nuget push "%%x" -k "%arg1%" -s "%arg2%"
+)
