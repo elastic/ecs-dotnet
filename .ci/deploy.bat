7c214d88c06d76eb303adea77bed6acc216ce2a5
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-02 17:13:09 +0000

    ci(jenkins): failfast if nuget push

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -4,5 +4,10 @@ set url=%2
 
 @echo on
 FOR /R ".\" %%a  in (*.nupkg) DO (
-	dotnet nuget push "%%a" -k "%key%" -s "%url%"
+	dotnet nuget push "%%a" -k "%key%" -s "%url%" && (
+		echo nuget push was successful
+	) || (
+		echo nuget push failed
+		exit 1
+	)
 )
