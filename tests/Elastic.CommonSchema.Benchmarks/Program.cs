0bcec185f4a8113d8efd5df8b8d475471025a19f
Martijn Laarman <Mpdreamz@gmail.com>
2020-06-02 15:39:28 +0200

    add benchmarks for simple serialization cases

---
+++ tests/Elastic.CommonSchema.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Elastic.CommonSchema.Benchmarks
+{
+	internal class Program
+	{
+		public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+	}
+}
