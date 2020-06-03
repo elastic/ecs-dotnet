82290a9b01ebbe9f2d600ca8cc0444e5cdb53997
Sly Gryphon <sly@gamertheory.net>
2020-06-03 19:19:51 +1000

    Move source code to root folder

---
+++ src/Elastic.CommonSchema.LoggerProvider/ElasticsearchLoggerOptionsSetup.cs
@@ -0,0 +1,18 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Elastic.CommonSchema
+{
+    internal class ElasticsearchLoggerOptionsSetup : ConfigureFromConfigurationOptions<ElasticsearchLoggerOptions>
+    {
+        public ElasticsearchLoggerOptionsSetup(
+            ILoggerProviderConfiguration<ElasticsearchLoggerProvider> providerConfiguration)
+            : base(providerConfiguration.Configuration)
+        {
+        }
+    }
+}
