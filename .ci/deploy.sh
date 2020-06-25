691d492e60da7a93f530770269eef238a4c575f0
Victor Martinez <victormartinezrubio@gmail.com>
2020-06-25 08:39:42 +0100

    [ci] fix dotnet nuget push duplicated and filter (#93)

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -7,5 +7,5 @@ set -euo pipefail
 while IFS= read -r -d '' nupkg
 do
     echo "dotnet nuget push ${nupkg}"
-    dotnet nuget push "${nupkg}" -k "${1}" -s "${2}"
-done < <(find . -type f -not -path './build/output/*' -name '*.nupkg' -print0)
+    dotnet nuget push "${nupkg}" -k "${1}" -s "${2}" --skip-duplicate
+done < <(find . -type f -path './build/output/*' -name '*.nupkg' -print0)
