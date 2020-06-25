2649f4ca46ab254fcac1f7901252f04a75c01a27
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-06-25 07:41:10 +0100

    [ci]: fix duplicated packages

--- .ci/deploy.bat
+++ .ci/deploy.bat
@@ -4,7 +4,7 @@ set url=%2
 
 @echo on
 FOR /R ".\" %%a  in (*.nupkg) DO (
-	dotnet nuget push "%%a" -k "%key%" -s "%url%" && (
+	dotnet nuget push "%%a" -k "%key%" -s "%url%" --skip-duplicate && (
 		echo nuget push was successful
 	) || (
 		echo nuget push failed
--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -7,5 +7,5 @@ set -euo pipefail
 while IFS= read -r -d '' nupkg
 do
     echo "dotnet nuget push ${nupkg}"
-    dotnet nuget push "${nupkg}" -k "${1}" -s "${2}"
+    dotnet nuget push "${nupkg}" -k "${1}" -s "${2}" --skip-duplicate
 done < <(find . -type f -not -path './build/output/*' -name '*.nupkg' -print0)
