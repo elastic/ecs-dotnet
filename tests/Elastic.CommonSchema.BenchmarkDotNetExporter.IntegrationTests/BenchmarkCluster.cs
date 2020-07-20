9f42b337d4698d3c2da78065bef97590f4dad564
Martijn Laarman <Mpdreamz@gmail.com>
2020-07-20 18:30:56 +0200

    dotnet test on sln file runs in parallel

--- tests/Elastic.CommonSchema.BenchmarkDotNetExporter.IntegrationTests/BenchmarkCluster.cs
+++ tests/Elastic.CommonSchema.BenchmarkDotNetExporter.IntegrationTests/BenchmarkCluster.cs
@@ -8,6 +8,6 @@ namespace Elastic.CommonSchema.BenchmarkDotNetExporter.IntegrationTests
 	/// <summary> Declare our cluster that we want to inject into our test classes </summary>
 	public class BenchmarkCluster : XunitClusterBase
 	{
-		public BenchmarkCluster() : base(new XunitClusterConfiguration("7.5.0")) { }
+		public BenchmarkCluster() : base(new XunitClusterConfiguration("7.5.0") { StartingPortNumber = 9202 }) { }
 	}
 }
--- tests/Elasticsearch.Extensions.Logging.IntegrationTests/LoggingCluster.cs
+++ tests/Elasticsearch.Extensions.Logging.IntegrationTests/LoggingCluster.cs
@@ -8,6 +8,6 @@ namespace Elasticsearch.Extensions.Logging.IntegrationTests
 	/// <summary> Declare our cluster that we want to inject into our test classes </summary>
 	public class LoggingCluster : XunitClusterBase
 	{
-		public LoggingCluster() : base(new XunitClusterConfiguration("7.7.0")) { }
+		public LoggingCluster() : base(new XunitClusterConfiguration("7.7.0") { StartingPortNumber = 9201 }) { }
 	}
 }
