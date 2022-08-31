94b7beed1e64c9987ea956c53324f37fcbc07116
Martijn Laarman <mpdreamz@gmail.com>
2022-08-31 22:25:49 +0200

    Expose service information to NLog and ensure Elastic.CommonSchema.NLog sets Service information

---
+++ src/Elastic.Apm.NLog/ApmServiceVersionLayoutRenderer.cs
@@ -0,0 +1,19 @@
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.LayoutRenderers;
+
+namespace Elastic.Apm.NLog;
+
+[LayoutRenderer(Name)]
+[ThreadSafe]
+public class ApmServiceVersionLayoutRenderer : LayoutRenderer
+{
+	public const string Name = "ElasticApmServiceVersion";
+
+	protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+	{
+		if (!Agent.IsConfigured) return;
+		builder.Append(Agent.Config.ServiceVersion);
+	}
+}
--- tests/Elastic.Apm.NLog.Test/NLogTests.cs
+++ tests/Elastic.Apm.NLog.Test/NLogTests.cs
@@ -12,6 +12,17 @@ namespace Elastic.Apm.NLog.Test
 {
 	public class NLogTests
 	{
+		public NLogTests()
+		{
+			var assembly = typeof(ApmTraceIdLayoutRenderer).Assembly;
+			global::NLog.Config.ConfigurationItemFactory.Default.RegisterItemsFromAssembly(assembly);
+
+			var configuration = new MockConfiguration("my-service", "my-service-node-name", "0.2.1");
+			if (!Agent.IsConfigured)
+				Agent.Setup(new AgentComponents(payloadSender: new NoopPayloadSender(), configurationReader: configuration));
+		}
+
+
 		/// <summary>
 		/// Creates 1 simple transaction and span and makes sure that the log line created within the transaction and span have
 		/// the transaction, trace and span ids, and logs prior to and after the transaction do not have those.
@@ -19,10 +30,6 @@ namespace Elastic.Apm.NLog.Test
 		[Fact]
 		public void NLogWithTransaction()
 		{
-			var assembly = typeof(ApmTraceIdLayoutRenderer).Assembly;
-			global::NLog.Config.ConfigurationItemFactory.Default.RegisterItemsFromAssembly(assembly);
-			Agent.Setup(new AgentComponents(payloadSender: new NoopPayloadSender()));
-
 			var target = new MemoryTarget();
 			target.Layout = "${ElasticApmTraceId}|${ElasticApmTransactionId}|${ElasticApmSpanId}|${message}";
 
@@ -56,6 +63,22 @@ namespace Elastic.Apm.NLog.Test
 			target.Logs[1].Should().Be($"{traceId}|{transactionId}||InTransaction");
 			target.Logs[2].Should().Be($"{traceId}|{transactionId}|{spanId}|InSpan");
 			target.Logs[3].Should().Be("|||PostTransaction");
+		}
+
+		[Fact]
+		public void NLogWithServiceName()
+		{
+			var target = new MemoryTarget();
+			target.Layout = "${ElasticApmServiceName}|${ElasticApmServiceNodeName}|${ElasticApmServiceVersion}|${message}";
+
+			global::NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
+
+			var logger = LogManager.GetLogger("Example");
+
+			logger.Debug("Some log");
+
+			target.Logs.Count.Should().Be(1);
+			target.Logs[0].Should().Be("my-service|my-service-node-name|0.2.1|Some log");
 		}
 	}
 }
