e6c243ce65b28840441eaecc0fd65f54ef32890c
Martijn Laarman <Mpdreamz@gmail.com>
2023-02-07 14:38:41 +0100

    Add generic config options to LoggerSinkConfiguration (#252)

--- src/Elastic.CommonSchema.Serilog.Sink/ElasticsearchSinkExtensions.cs
+++ src/Elastic.CommonSchema.Serilog.Sink/ElasticsearchSinkExtensions.cs
@@ -7,11 +7,12 @@ namespace Elastic.CommonSchema.Serilog.Sink
 {
 	public static class ElasticsearchSinkExtensions
 	{
-		public static LoggerConfiguration Elasticsearch(
-			this LoggerSinkConfiguration loggerConfiguration,
-			ElasticsearchSchemaSinkOptions? options = null
-		) =>
+		public static LoggerConfiguration Elasticsearch(this LoggerSinkConfiguration loggerConfiguration, ElasticsearchSchemaSinkOptions? options = null) =>
 			loggerConfiguration.Sink(new ElasticsearchSink(options ?? new ElasticsearchSchemaSinkOptions()));
+
+		public static LoggerConfiguration Elasticsearch<TEcsDocument>(this LoggerSinkConfiguration loggerConfiguration, ElasticsearchSchemaSinkOptions<TEcsDocument>? options = null)
+			where TEcsDocument : EcsDocument, new() =>
+			loggerConfiguration.Sink(new ElasticsearchSink<TEcsDocument>(options ?? new ElasticsearchSchemaSinkOptions<TEcsDocument>()));
 
 		public static LoggerConfiguration Elasticsearch(
 			this LoggerSinkConfiguration loggerConfiguration,
@@ -26,6 +27,19 @@ namespace Elastic.CommonSchema.Serilog.Sink
 			return loggerConfiguration.Sink(new ElasticsearchSink(sinkOptions));
 		}
 
+		public static LoggerConfiguration Elasticsearch<TEcsDocument>(
+			this LoggerSinkConfiguration loggerConfiguration,
+			ICollection<Uri> nodes,
+			Action<ElasticsearchSchemaSinkOptions<TEcsDocument>>? configureOptions = null,
+			bool useSniffing = true
+		) where TEcsDocument : EcsDocument, new()
+		{
+			var sinkOptions = new ElasticsearchSchemaSinkOptions<TEcsDocument>(useSniffing ? TransportHelper.Static(nodes) : TransportHelper.Sniffing(nodes));
+			configureOptions?.Invoke(sinkOptions);
+
+			return loggerConfiguration.Sink(new ElasticsearchSink<TEcsDocument>(sinkOptions));
+		}
+
 		public static LoggerConfiguration ElasticCloud(
 			this LoggerSinkConfiguration loggerConfiguration,
 			string endpoint,
@@ -37,6 +51,18 @@ namespace Elastic.CommonSchema.Serilog.Sink
 			configureOptions?.Invoke(sinkOptions);
 
 			return loggerConfiguration.Sink(new ElasticsearchSink(sinkOptions));
+		}
+		public static LoggerConfiguration ElasticCloud<TEcsDocument>(
+			this LoggerSinkConfiguration loggerConfiguration,
+			string endpoint,
+			string apiKey,
+			Action<ElasticsearchSchemaSinkOptions<TEcsDocument>>? configureOptions = null
+		) where TEcsDocument : EcsDocument, new()
+		{
+			var sinkOptions = new ElasticsearchSchemaSinkOptions<TEcsDocument>(TransportHelper.Cloud(endpoint, apiKey));
+			configureOptions?.Invoke(sinkOptions);
+
+			return loggerConfiguration.Sink(new ElasticsearchSink<TEcsDocument>(sinkOptions));
 		}
 
 		public static LoggerConfiguration ElasticCloud(
@@ -51,6 +77,20 @@ namespace Elastic.CommonSchema.Serilog.Sink
 			configureOptions?.Invoke(sinkOptions);
 
 			return loggerConfiguration.Sink(new ElasticsearchSink(sinkOptions));
+		}
+
+		public static LoggerConfiguration ElasticCloud<TEcsDocument>(
+			this LoggerSinkConfiguration loggerConfiguration,
+			string endpoint,
+			string username,
+			string password,
+			Action<ElasticsearchSchemaSinkOptions<TEcsDocument>>? configureOptions = null
+		) where TEcsDocument : EcsDocument, new()
+		{
+			var sinkOptions = new ElasticsearchSchemaSinkOptions<TEcsDocument>(TransportHelper.Cloud(endpoint, username, password));
+			configureOptions?.Invoke(sinkOptions);
+
+			return loggerConfiguration.Sink(new ElasticsearchSink<TEcsDocument>(sinkOptions));
 		}
 	}
 }
