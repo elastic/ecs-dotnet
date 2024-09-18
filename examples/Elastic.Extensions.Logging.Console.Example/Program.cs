9ec7e0ce6ff41210f0bf019fc4a243818cdc8baa
Martijn Laarman <Mpdreamz@gmail.com>
2024-09-18 16:38:18 +0200

    Introduce Console formatter for Microsoft.Extensions.Logging.Console" (#439)
    - Introduce Console formatter Microsoft.Extensions.Logging
    - Add README contents
    
    ---------
    
    Co-authored-by: Steve Gordon <sgordon@hotmail.co.uk>

---
+++ examples/Elastic.Extensions.Logging.Console.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Elastic.Extensions.Logging.Console;
+using Elastic.Extensions.Logging.Console.Example;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+await Host.CreateDefaultBuilder(args)
+	.UseConsoleLifetime()
+	.ConfigureAppConfiguration((_, configurationBuilder) =>
+	{
+		configurationBuilder.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+	})
+	.ConfigureLogging((_, loggingBuilder) => loggingBuilder.AddEcsConsole())
+	.ConfigureServices((_, services) =>
+	{
+		services.AddHostedService<ExampleService>();
+	})
+	.Build()
+	.RunAsync();
+
---
+++ src/Elastic.Extensions.Logging.Common/README.md
@@ -0,0 +1,5 @@
+# Elastic.Extensions.Logging.Common
+
+Transitive dependency for `Elastic.Extensions.Logging` and `Elastic.Extensions.Logging.Console`.
+
+Should not be installed directly.
